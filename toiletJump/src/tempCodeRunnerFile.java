d09faa7dec61beb2876812ca6b0c21eeff842b88
JeffTimoman <ppti.jeffersontimotius@gmail.com>
2023-05-16 00:21:45 +0700

    Final Update

--- toiletJump/src/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-new Timer(1000 / 60, new ActionListener() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {
-        //         movePipes();
-        //         removePipes();
-        //         repaint();
-        //     }
-        // });
-        // timer.start();
